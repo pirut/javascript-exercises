64feca4652d64e38a0db57f73a62245e2d714a6c
JR Bussard <scottbussardjr@gmail.com>
2024-07-14 19:30:52 -0400

    Solved the reverseString Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let output = "";
 
+  for (let i = string.length; i > 0; i--) {
+    output += string.charAt(i - 1);
+  }
+
+  return output;r
 };
 
 // Do not edit below this line
