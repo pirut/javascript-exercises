ed4e5863928d5fc4bcf13a966347fcc8ff999e66
JR Bussard <scottbussardjr@gmail.com>
2024-07-14 19:58:59 -0400

    Solved the removeFromArray Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(refArray, ...removalArgs) {
+    let output = [];
+
+    for (const refItem of refArray) {
+        if (removalArgs.includes(refItem)) {
+            continue;
+        }
+        else {
+            output.push(refItem);
+            continue;
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
