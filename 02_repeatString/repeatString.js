2c5414159fde24b42a72c3eda8f23aa645af983c
JR Bussard <scottbussardjr@gmail.com>
2024-07-14 15:05:56 -0400

    Finished the repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, quantity) {
 
+    let output = ""
+
+    if (quantity < 0 || typeof string !== "string" ) {
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < quantity; i++) {
+            output += string;
+        }
+        return output;
+    }
 };
 
 // Do not edit below this line
