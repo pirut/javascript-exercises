e9655d70a6ce419a289aca3ae36c6547df1306f9
JR Bussard <scottbussardjr@gmail.com>
2024-07-16 11:29:10 -0400

    Solved the sumAll Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  let output = 0;
+  if (
+    typeof firstNumber !== "number" ||
+    typeof secondNumber !== "number" ||
+    firstNumber < 0 ||
+    secondNumber < 0
+  ) {
+    return "ERROR";
+  } else {
+    if (firstNumber > secondNumber) {
+      let temp = firstNumber;
+      firstNumber = secondNumber;
+      secondNumber = temp;g
+    }
+    for (let adder = firstNumber; adder <= secondNumber; adder++) {
+        output += adder;
+    }
+    return output;
+  }
 };
 
 // Do not edit below this line
