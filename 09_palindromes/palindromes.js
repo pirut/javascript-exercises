b4ee8f85abb26a518bc4db3071146e8532ba1bf4
JR Bussard <scottbussardjr@gmail.com>
2024-08-02 18:09:07 -0400

    Solved the Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const stringArr = string
+        .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, "")
+        .toLowerCase()
+        .split("");
+    const stringArrRev = [...stringArr].reverse();
+    let returnValue = true;
+    for (let index = 0; index < stringArr.length; index++) {
+        if (stringArr[index] === stringArrRev[index]) {
+            continue;
+        } else {
+            returnValue = false;
+            return returnValue;
+        }
+    }
+    return returnValue;
 };
+
+// console.log(palindromes("ZZZZ car, a man, a maracaz."));
 
 // Do not edit below this line
 module.exports = palindromes;
