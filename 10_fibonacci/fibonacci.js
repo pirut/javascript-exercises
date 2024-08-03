b1324f32909de174a07d7bcdea8355c9d8ce62da
JR Bussard <scottbussardjr@gmail.com>
2024-08-02 21:21:35 -0400

    Solved the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    number = parseInt(number);
+    let total = 0;
+    let past = 0;
+    let next = 1;
+    if (number < 0) {
+        return "OOPS";
+    } else if (number === 1) {
+        return 1;
+    } else {
+        for (let i = 1; i < number; i++) {
+            total = past + next;
+            past = next;
+            next = total;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
