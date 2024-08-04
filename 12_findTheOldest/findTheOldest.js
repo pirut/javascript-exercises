2a37b9c00a278387a5b4751488651ac64dc3e996
JR Bussard <scottbussardjr@gmail.com>
2024-08-03 21:34:27 -0400

    solved the find oldest problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    arr.map((person) => {
+        const birth = person.yearOfBirth;
+        let death;
+        person.yearOfDeath === undefined ? (death = new Date().getFullYear()) : (death = person.yearOfDeath);
+        person.age = death - birth;
+    });
+    return arr.reduce((largest, current) => {
+        if (largest.age > current.age) {
+            return largest;
+        } else {
+            largest = current;
+            return largest;
+        }
+    });
 };
 
 // Do not edit below this line
